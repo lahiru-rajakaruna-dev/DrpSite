e57d21e471f71d096ac2f6f3264b958985a0ddae
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-21 12:53:30 +0530

    vendor-section: checkout button

---
+++ src/app/[user]/[product]/(VendorSection)/CheckoutButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button';
+
+
+
+export default function CheckoutButton() {
+    return <div>
+        <Button
+            className={ 'w-fit h-fit px-4! py-3! text-lg' +
+                        ' font-semibold' }
+        >
+            <pre>Buy Now</pre>
+        </Button>
+    </div>
+}
+
+
