4e91fd1139e9300ccd62aa6fae2703491664df80
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-23 14:00:33 +0530

    updated drizzle-kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
+import * as dotenv      from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
 
 
 
+dotenv.config({
+				  path: './.env.development'
+			  })
+
 export default defineConfig({
-								schema : './drizzle/schema',
-								dialect: 'postgresql',
-								casing : 'snake_case',
-								out    : './drizzle_migrations',
-								verbose: true,
-								
+								schema       : './drizzle/schema',
+								dialect      : 'postgresql',
+								casing       : 'snake_case',
+								out          : './drizzle_migrations',
+								verbose      : true,
+								dbCredentials: {
+									url: process.env.DATABASE_URL!,
+								}
 							})
