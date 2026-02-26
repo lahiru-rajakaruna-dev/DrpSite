30baf9f6f8e94df2abafb0ade1c859be38b1325a
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-26 14:01:58 +0530

    feat(orm module): add orm factory

---
+++ src/app/api/_modules/orm_module/orm.factory.ts
@@ -0,0 +1,35 @@
+import {DrizzleOrmService}          from "@/app/api/_modules/orm_module/drizzle.orm";
+import {IOrmService, ORM_PROVIDERS} from "@/app/api/_modules/orm_module/orm.interface";
+import * as dotenv                  from 'dotenv'
+import path                         from "node:path";
+
+
+
+dotenv.config({
+				  path: [
+					  path.resolve(
+						  process.cwd(),
+						  './env.development',
+					  ),
+					  path.resolve(
+						  process.cwd(),
+						  './env.production',
+					  )
+				  ]
+			  })
+
+
+
+export class OrmServiceFactory {
+	public static async getService(): Promise<IOrmService> {
+		const ormOption = process.env.ORM_PROVIDER
+
+		switch (ormOption) {
+			case 'DRIZZLE':
+				return await DrizzleOrmService.getInstance()
+			default:
+				throw new Error(`Invalid orm option.
+ Available Options: ${ORM_PROVIDERS.join(' | ')}`)
+		}
+	}
+}
