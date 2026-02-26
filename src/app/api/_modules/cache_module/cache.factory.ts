398f24db24fa9e240a0cf82bfaf772618ab64f90
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-26 10:37:36 +0530

    style(cache module): formatting updates

--- src/app/api/_modules/cache_module/cache.factory.ts
+++ src/app/api/_modules/cache_module/cache.factory.ts
@@ -1,34 +1,33 @@
-import * as dotenv        from 'dotenv'
-import path               from 'node:path';
-import {
-    CACHE_PROVIDERS,
-    ICacheService
-}                         from './cache.interface';
-import {NodeCacheService} from "./node_cache.service";
-import {LRUCacheService}  from "./lru_cache.service";
+import * as dotenv                      from 'dotenv'
+import path                             from 'node:path';
+import {CACHE_PROVIDERS, ICacheService} from './cache.interface';
+import {LRUCacheService}                from "./lru_cache.service";
+import {NodeCacheService}               from "./node_cache.service";
+
 
 
 dotenv.config({
-                  debug: true,
-                  path : [
-                      path.resolve(process.cwd(), '.env.development'),
-                      path.resolve(process.cwd(), '.env.production')
-                  ],
-              })
+				  debug: true,
+				  path : [
+					  path.resolve(process.cwd(), '.env.development'),
+					  path.resolve(process.cwd(), '.env.production')
+				  ],
+			  })
+
 
 
 export class CacheServiceFactory {
-    public getCacheService(): ICacheService {
-        const cache_provider = process.env.CACHE_PROVIDER
+	public getCacheService(): ICacheService {
+		const cache_provider = process.env.CACHE_PROVIDER
 
-        switch (cache_provider) {
-            case 'NODE_CACHE':
-                return NodeCacheService.getInstance()
-            case 'LRU':
-                return LRUCacheService.getInstance();
-            default:
-                throw new Error(`Invalid cache provider: ${cache_provider}\nAvailable Options:${CACHE_PROVIDERS.join(
-                    ' | ')}`)
-        }
-    }
+		switch (cache_provider) {
+			case 'NODE_CACHE':
+				return NodeCacheService.getInstance()
+			case 'LRU':
+				return LRUCacheService.getInstance();
+			default:
+				throw new Error(`Invalid cache provider: ${cache_provider}\nAvailable Options:${CACHE_PROVIDERS.join(
+					' | ')}`)
+		}
+	}
 }
