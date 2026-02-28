5b1ba33c4a9209c5def5bcb35843efa566b1356a
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-28 22:45:45 +0530

    style(auth module): renamed file

--- src/app/api/_modules/auth_module/base.auth.service.ts
+++ src/app/api/_modules/auth_module/base.auth.service.ts
@@ -1,12 +1,10 @@
 import {IAuthService}   from "@/app/api/_modules/auth_module/auth.interface";
 import {ILoggerService} from "@/app/api/_modules/logger_module/logger.interface";
-import {NextApiRequest} from "next";
-import {NextResponse}   from "next/server";
 
 
 
 export abstract class BaseAuthService implements IAuthService {
-	protected static auth_service_instance: IAuthService;
+	protected static instance: BaseAuthService;
 	protected readonly logger: ILoggerService;
 
 	protected constructor(logger: ILoggerService) {
