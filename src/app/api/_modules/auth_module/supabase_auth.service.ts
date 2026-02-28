6e7b6fc3419405cb9193a4ddc170a1ed61748d18
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-28 22:32:49 +0530

    refactor(auth module): update method name

--- src/app/api/_modules/auth_module/supabase_auth.service.ts
+++ src/app/api/_modules/auth_module/supabase_auth.service.ts
@@ -1,12 +1,13 @@
-import {BaseAuthService}              from "@/app/api/_modules/auth_module/abstract_auth.service";
-import {LoggerServiceFactory}         from "@/app/api/_modules/logger_module/logger.factory";
-import {createClient, SupabaseClient} from "@supabase/supabase-js";
+import {BaseAuthService}      from "@/app/api/_modules/auth_module/abstract_auth.service";
+import {LoggerServiceFactory} from "@/app/api/_modules/logger_module/logger.factory";
+import {
+	createClient,
+	SupabaseClient
+}                             from "@supabase/supabase-js";
 
-import * as dotenv                 from 'dotenv'
-import {NextApiRequest}            from "next";
-import {cookies}                   from "next/headers";
-import {NextRequest, NextResponse} from "next/server";
-import path                        from "node:path";
+import * as dotenv   from 'dotenv'
+import {NextRequest} from "next/server";
+import path          from "node:path";
 
 
 
@@ -32,14 +33,16 @@ export class SupabaseAuthService extends BaseAuthService {
 		const SUPABASE_KEY = process.env.SUPABASE_KEY
 
 		if (!this.auth_service_instance) {
-			this.auth_service_instance = new SupabaseAuthService(SUPABASE_URL!, SUPABASE_KEY!)
+			this.auth_service_instance = new SupabaseAuthService(
+				SUPABASE_URL!,
+				SUPABASE_KEY!
+			)
 		}
 
 		return this.auth_service_instance
 	}
 
-
-	authorizeRequest(request: Request): boolean {
+	canProceed(request: Request): boolean {
 		const parsedReq = new NextRequest(new URL(request.url), request)
 
 		return true;
