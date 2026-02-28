f556aca9d1787429250a8307cc459d7a3237224f
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-28 16:01:59 +0530

    feat(orm module): add page orm extender

---
+++ src/app/api/_modules/orm_module/extenders/page.orm.extender.ts
@@ -0,0 +1,167 @@
+import {BaseOrmService} from "@/app/api/_modules/orm_module/abstract.orm";
+import {
+	OrmError,
+	TDrizzleOrm
+}                       from "@/app/api/_modules/orm_module/drizzle.orm";
+import {
+	table_pages,
+	TInsertPage,
+	TSelectPage,
+	TUpdatePage
+}                       from "@drizzle/schema";
+import {eq}             from "drizzle-orm";
+import {and}            from "drizzle-orm/sql/expressions/conditions";
+
+
+
+export class PageExtendedOrm {
+
+	private static instance: PageExtendedOrm | undefined;
+	private readonly base_orm: BaseOrmService<TDrizzleOrm>;
+
+	constructor(baseOrm: BaseOrmService<TDrizzleOrm>) {
+		this.base_orm = baseOrm
+
+	}
+
+	async createPage(
+		userID: string,
+		page: TInsertPage
+	): Promise<TSelectPage> {
+		try {
+			const result = await this.base_orm.driver.insert(table_pages)
+									 .values({...page, page_owner_id: userID})
+									 .returning()
+			return this.base_orm.logger.logAndReturn(
+				result[0],
+				'operation: create_page'
+			);
+		} catch (e) {
+			throw new OrmError(
+				(e as Error).message,
+				'create_page',
+				{data: page}
+			)
+		}
+	}
+
+	async getPageById(
+		userID: string,
+		id: string
+	): Promise<TSelectPage | undefined> {
+		try {
+			const result = await this.base_orm.driver.query.table_pages.findFirst({
+																					  where(columns) {
+																						  return and(
+																							  eq(
+																								  columns.page_owner_id,
+																								  userID
+																							  ),
+																							  eq(
+																								  columns.page_id,
+																								  id
+																							  )
+																						  )
+																					  }
+																				  })
+
+			return this.base_orm.logger.logAndReturn(
+				result,
+				'operation: get_page_by_id'
+			)
+		} catch (e) {
+			throw new OrmError(
+				(e as Error).message,
+				'get_page_by_id',
+				{id: id}
+			)
+		}
+	}
+
+	async getPagesByUserId(userId: string): Promise<TSelectPage[]> {
+		try {
+			const result = await this.base_orm.driver.query.table_pages.findMany({
+																					 where(columns) {
+																						 return eq(
+																							 columns.page_owner_id,
+																							 userId
+																						 )
+																					 }
+																				 })
+
+			return this.base_orm.logger.logAndReturn(
+				result,
+				'operation: get_pages_by_user_id'
+			)
+		} catch (e) {
+			throw new OrmError(
+				(e as Error).message,
+				'get_pages_by_user_id',
+				{user_id: userId}
+			)
+		}
+	}
+
+	async updatePageById(
+		userID: string,
+		id: string,
+		pageUpdates: TUpdatePage
+	): Promise<TSelectPage> {
+		try {
+			const result = await this.base_orm.driver.update(table_pages)
+									 .set(pageUpdates)
+									 .where(
+										 and(
+											 eq(
+												 table_pages.page_id,
+												 id
+											 ),
+											 eq(
+												 table_pages.page_owner_id,
+												 userID
+											 )
+										 ))
+									 .returning()
+
+			return this.base_orm.logger.logAndReturn(
+				result[0],
+				'operation: update_page_by_id'
+			)
+		} catch (e) {
+			throw new OrmError(
+				(e as Error).message,
+				'update_page_by_id',
+				{id: id, updates: pageUpdates}
+			)
+		}
+	}
+
+	async deletePageById(
+		userID: string,
+		id: string
+	): Promise<boolean> {
+		try {
+			const result = await this.base_orm.driver.delete(table_pages)
+									 .where(
+										 and(
+											 eq(
+												 table_pages.page_id,
+												 id
+											 ),
+											 eq(
+												 table_pages.page_owner_id,
+												 userID
+											 )
+										 ))
+									 .returning()
+			this.base_orm.logger.log(JSON.stringify(result[0]))
+			return true
+		} catch (e) {
+			throw new OrmError(
+				(e as Error).message,
+				'delete_user',
+				{id: id}
+			)
+		}
+	}
+}
