61d577572ba9645faf63765a7a23b4e09a5c8e19
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-06 17:51:14 +0530

    refactor(repository module): add interfaces for individual extenders

--- src/app/api/_modules/repository_module/repository.interface.ts
+++ src/app/api/_modules/repository_module/repository.interface.ts
@@ -1,7 +1,10 @@
-export interface IRepository<TSelect, TUpdate, TInsert> {
-	createOne(userID: string, data: TInsert): Promise<TSelect>
+export interface IRepository<TInsert, TSelect, TUpdate> {
+	createOne(data: TInsert): Promise<TSelect>
 
-	getOne(userID: string, id: string): Promise<TSelect | undefined>
+	getOne(
+		userID: string,
+		id: string
+	): Promise<TSelect | undefined>
 
 	getAll(userID: string): Promise<TSelect[]>
 
@@ -12,4 +15,33 @@ export interface IRepository<TSelect, TUpdate, TInsert> {
 	): Promise<TSelect | undefined>
 
 	deleteOne(userID: string, id: string): Promise<boolean>
+}
+
+
+
+export interface IUserManagable<TInsert, TSelect, TUpdate> {
+
+	createUser(data: TInsert): Promise<TSelect>
+
+	getUser(id: string): Promise<TSelect | undefined>
+
+	getAllUsers(): Promise<TSelect[]>
+
+	updateUser(id: string, updates: TUpdate): Promise<TSelect | undefined>
+
+	deleteUser(id: string): Promise<boolean>
+}
+
+
+
+export interface IProductManagable<TInsert, TSelect, TUpdate> {
+	createProduct(data: TInsert): Promise<TSelect>
+
+	getProduct(userID: string, id: string): Promise<TSelect>
+
+	getAllProducts(userID: string): Promise<TSelect[]>
+
+	updateProduct(userID: string, id: string): Promise<TSelect>
+
+	deleteProduct(userID: string, id: string): Promise<boolean>
 }
